b87e9261ce977c774965573f96a70c0588dc919d
Marcos <exegeses@gmail.com>
2024-03-21 13:08:21 -0300

    Concepto de variables y constantes | innerText | prompt()

---
+++ clase2/usuario.js
@@ -0,0 +1,11 @@
+/*
+    - Pedimos al usuario que ingrese su nombre
+    - Almacenamos ese dato en memoria
+*/
+
+let nombre = prompt('ingrese su nombre');
+
+// imprimimos el nombre del usuario en el documento
+//document.write( 'Bienvenido ', nombre);
+
+frase.innerText = nombre;
---
+++ clase2/variables.js
@@ -0,0 +1,31 @@
+/*
+    Para declarar variables 
+    utilizamos la palabra reservada "let"
+*/
+let numero = 10;
+let frase = 'hola mundo';
+/*
+    Para declarar constantes
+    utilizamos la palabra reservada "const"
+  
+*/
+const nombre = 'Marcos';
+
+
+// imprimimos valor de nuestras variables en el documento
+document.write( numero );
+document.write( '<br>' );
+document.write( frase );
+document.write( '<br>' );
+// sobre escribimos el valor de la variable número
+numero = 20;
+document.write( numero );
+frase = 'Javascript desde 0';
+document.write( '<br>' );
+document.write( frase );
+document.write( '<br>' );
+// Imprimimos la constante nombre
+document.write( nombre );
+
+//nombre = 'jgd';
+/* No se puede sobre escribir una constante */
